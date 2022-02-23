7ee8c7322e080a04ab4bbbc380a542470f66d43e
你家臭狗屎 <42485691+AutoCONFIG@users.noreply.github.com>
2022-02-23 13:06:04 +0800

    Add files via upload

---
+++ include/minieap_common.h
@@ -0,0 +1,22 @@
+#ifndef _MINIEAP_MINIEAP_COMMON_H
+#define _MINIEAP_MINIEAP_COMMON_H
+
+#define VERSION "0.93.7-beta2"
+
+typedef enum _function_result {
+    SUCCESS = 0,
+    FAILURE = -1
+} RESULT;
+
+#define IS_FAIL(x) ((x) == FAILURE)
+
+/* Line buffer length when reading files */
+#define MAX_LINE_LEN 100
+
+/* Max path length */
+#define MAX_PATH 260
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
